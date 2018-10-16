8a73785b81ba2b622fc7eb62557c5ecd44506e89
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-16 19:44:35 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -4,6 +4,8 @@ class RepositoriesController < ApplicationController
     user = Faraday.get 'https://api.github.com/user' do |req|
       req.headers['Authorization'] = 'token ' + session[:token]
       req.headers['Accept'] = 'application/json'
+    end 
+    @user_data = JSON.parse(user.body)
   end
 
   def create
