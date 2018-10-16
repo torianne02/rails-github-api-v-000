59e4a5fb0a41f31fe5db275da38c9c27c0977d9f
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-16 19:19:22 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
+    client_id = ENV['GITHUB_CLIENT_ID']
+    client_secret = ENV['GITHUB_CLIENT_SECRET']
   end
 end
