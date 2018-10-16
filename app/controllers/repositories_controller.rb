dd7f977161fa73316c04b9dca401860904a2f27d
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-16 19:44:08 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,9 @@
 class RepositoriesController < ApplicationController
 
   def index
+    user = Faraday.get 'https://api.github.com/user' do |req|
+      req.headers['Authorization'] = 'token ' + session[:token]
+      req.headers['Accept'] = 'application/json'
   end
 
   def create
@@ -11,5 +14,4 @@ class RepositoriesController < ApplicationController
     end
     redirect_to '/'
   end
-
 end
